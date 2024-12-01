690a7851fe0f325d69b8e40e102ea0ad931f0ab7
MatveevDaniil <dmatveev@sfsu.edu>
2024-12-01 11:40:04 -0800

    changing parallelism

--- src/sim_search_part_patterns.hpp
+++ src/sim_search_part_patterns.hpp
@@ -36,6 +36,7 @@ inline void check_part(
   #pragma omp parallel 
   {
   double wtime = omp_get_wtime();
+  int thread_id = omp_get_thread_num();
   #pragma omp for schedule(guided)
   for (size_t i = 0; i < entries_small.size(); ++i) {
     const auto* entry = entries_small[i];
