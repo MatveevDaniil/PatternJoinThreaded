9ac94fed170ee3f3ec1f7a23debfdb55242d3efd
MatveevDaniil <dmatveev@sfsu.edu>
2024-12-08 00:49:39 -0800

    test setup

--- performance_testing/threaded_hash_maps.cpp
+++ performance_testing/threaded_hash_maps.cpp
@@ -1,5 +1,6 @@
 #include <omp.h>
 #include <vector>
+#include <array>
 #include <unordered_set>
 #include "../thirdparty/unordered_dense.h"
 #include "../thirdparty/gtl/phmap.hpp"
@@ -257,6 +258,120 @@ int mapreduce_semipattern_search(
 }
 
 
+template <int P>
+int mapreduce_semipattern_search2(
+  std::vector<std::string> input,
+  const std::string& map_name,
+  std::ofstream& ofs,
+  size_t true_output_size
+) {
+  gtl::parallel_flat_hash_map_m<std::string, std::array<idx_vector, P>> map;
+  gtl_p_set_idxpair output;
+  std::string N = std::to_string(input.size());
+  std::string P_str = std::to_string(P);
+  tbb::concurrent_vector<std::string> patterns_vector;
+
+  measure_time(ofs, N + "," + map_name + ",insert," + P_str, [&]() {
+    #pragma omp parallel num_threads(P) 
+    {
+      int tid = omp_get_thread_num();
+      #pragma omp for
+      for (size_t i = 0; i < input.size(); ++i) {
+        const std::string& str = input[i];
+        for (const auto& pattern : semi2Patterns(str))
+          map[pattern][tid].push_back(i);
+      }
+    }
+    for (auto& [pattern, _]: map) {
+      patterns_vector.push_back(pattern);
+    }
+  });
+
+
+  measure_time(ofs, N + "," + map_name + ",iteration," + P_str, [&]() {
+    #pragma omp parallel num_threads(P) 
+    {
+      #pragma omp for schedule(dynamic, 10)
+      for (size_t i = 0; i < patterns_vector.size(); i++) {
+        auto& pattern = patterns_vector[i];
+        std::vector<idx_vector*> loc_idxs_vec;
+        loc_idxs_vec.reserve(P);
+        for (auto& loc_idxs : map[pattern]) {
+          loc_idxs_vec.push_back(&loc_idxs);
+        }
+        for (size_t I = 0; I < loc_idxs_vec.size(); I++) {
+          auto& idxs = *loc_idxs_vec[I];
+          for (size_t i = 0; i < idxs.size(); ++i)
+            for (size_t j = i + 1; j < idxs.size(); ++j)
+              if (idxs[i] != idxs[j]) 
+                if (edit_distance_k(input[idxs[i]], input[idxs[j]], 2)) {
+                  if (idxs[i] < idxs[j])
+                    output.insert({idxs[i], idxs[j]});
+                  else
+                    output.insert({idxs[j], idxs[i]});
+                }
+          for (size_t J = I + 1; J < loc_idxs_vec.size(); J++) {
+            auto& idxs2 = *loc_idxs_vec[J];
+            for (size_t i = 0; i < idxs.size(); ++i)
+              for (size_t j = 0; j < idxs2.size(); ++j)
+                if (idxs[i] != idxs2[j]) 
+                  if (edit_distance_k(input[idxs[i]], input[idxs2[j]], 2)) {
+                    if (idxs[i] < idxs2[j])
+                      output.insert({idxs[i], idxs2[j]});
+                    else
+                      output.insert({idxs2[j], idxs[i]});
+                  }
+          }
+        }
+      }
+    }
+  });
+  size_t output_size = output.size() + input.size();
+  if (output_size != true_output_size) {
+    std::cout << "the output size " << output_size << " is incorrect" << std::endl;
+    std::ofstream ofs("check_output");
+    for (const auto& [i, j] : output) {
+      ofs << input[i] << " " << input[j] << std::endl;
+    }
+    throw std::runtime_error("calculation error");
+  }
+
+  measure_time(ofs, N + "," + map_name + ",map_clear," + P_str, [&]() {
+    map.clear();
+    patterns_vector.clear();
+  });
+
+  return 0;
+}
+
+void mapreduce_semipattern_search2(
+  std::vector<std::string> input,
+  const std::string& map_name,
+  std::ofstream& ofs,
+  int P,
+  size_t true_output_size
+) {
+  if (P == 1) {
+    mapreduce_semipattern_search2<1>(
+      input, map_name, ofs, true_output_size);
+  } else if (P == 2) {
+    mapreduce_semipattern_search2<2>(
+      input, map_name, ofs, true_output_size);
+  } else if (P == 4) {
+    mapreduce_semipattern_search2<4>(
+      input, map_name, ofs, true_output_size);
+  } else if (P == 8) {
+    mapreduce_semipattern_search2<8>(
+      input, map_name, ofs, true_output_size);
+  } else if (P == 16) {
+    mapreduce_semipattern_search2<16>(
+      input, map_name, ofs, true_output_size);
+  } else {
+    std::cout << "add P=" << P << " to the wrapper" << std::endl;
+  }
+}
+
+
 int main() {
   std::vector<std::string> TEST_FILES = {"../test_data/P00245-aa"};
   std::vector<size_t> true_outputs = {14802311};
@@ -292,6 +407,8 @@ int main() {
     readFile(TEST_FILES[i], strings);
 
     for (int P : threads) {
+      mapreduce_semipattern_search2(
+        strings, "gtl_mapreduce2", ofs_p, P, true_outputs[i]);
       mapreduce_semipattern_search<ankerl_map>(
         strings, "ankerl_mapreduce", ofs_p, P, true_outputs[i]);
       // phmap_semipattern_search<gtl_p_map>(
